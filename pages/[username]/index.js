2005654c9f1c1211c5a7f20310b0fe6882e04f3c
Omkar Rajwade <orajwade@gmail.com>
2021-12-28 10:26:32 +0530

    Time to ship it

---
+++ pages/[username]/index.js
@@ -0,0 +1,41 @@
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+export async function getServerSideProps({ query }) {
+  const { username } = query;
+  const userDoc = await getUserWithUsername(username);
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+  // If no user, short circuit to 404 page
+  if (!userDoc) {
+    return {
+      notFound: true,
+    };
+  }
+
+  if (userDoc) {
+    user = userDoc.data();
+    console.log(user);
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </main>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
+import { UserContext } from "../lib/context";
+import { useEffect, useState } from "react";
+import { useUserData } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  let userData = useUserData();
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
